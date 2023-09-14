cc1328b3bfe85ea8cb93d9d2fb584839d2094d0b
NicholasTysh <s01300805@acad.tri-c.edu>
2023-09-14 11:15:48 -0400

    init Lab1

---
+++ Lab1/index.php
@@ -0,0 +1,17 @@
+<?php
+    $name = "Nick";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lab1</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <main>
+        <h1>
+            Hello World, from <?php echo $name; ?>
+        </h1>
+    </main>
+</body>
+</html>
